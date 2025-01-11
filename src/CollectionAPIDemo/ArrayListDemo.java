6393ab3e14cbe7548ae91b1adefb5a90029a019d
sumukhanidhibs <sumukhanidhi@gmail.com>
2025-01-11 14:55:06 +0530

    ArrayList Added

--- src/CollectionAPIDemo/ArrayListDemo.java
+++ src/CollectionAPIDemo/ArrayListDemo.java
@@ -1,7 +1,26 @@
 package CollectionAPIDemo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayListDemo {
     public static void main(String[] args) {
+        ArrayList<Integer> ar = new ArrayList<>();
+//        boolean a=true;
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String n = sc.nextLine();
+            if(n.isEmpty()){
+                break;
+            }else{
+                ar.add(Integer.parseInt(n));
+            }
+        }
+
+        for(Integer i : ar){
+            System.out.println(i);
+        }
 
     }
 }
